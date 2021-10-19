784dd8fe4be26860a2143fe724cd2534aabf30d5
Lauren Bullen <lbullen@healthline.com>
2021-10-18 21:06:05 -0700

    partially working slideshow

--- script.js
+++ script.js
@@ -20,3 +20,40 @@ function closeMenu() {
 
 //form validation
 
+
+//slideshow functionality
+let slideIndex = 1
+showSlides(slideIndex)
+
+//shows previous/next slide
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+//shows current slide
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName("mySlides")
+    let dots = document.getElementsByClassName("dot")
+
+    if(n>slideIndex){
+        slideIndex = 1
+    } else if(n < 1){
+        slideIndex = slides.length
+    }
+
+    for(i = 0; i<slides.length; i++){
+        slides[i].style.display = "none"
+    }
+
+    for(i = 0; i<dots.length ; i++){
+        dots[i].className = dots[i].className.replace("active", " ")
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += "  active"
+
+}
